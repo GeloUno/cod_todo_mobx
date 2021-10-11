5c9e41373879f45720b5de92ddd71a4641776ee0
GeloUno <geloke59@gmail.com>
2021-10-11 17:59:31 +0200

    add form to layout

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -4,6 +4,7 @@ import { mystore } from '../store/todos';
 import { Button } from '../components/todoList';
 // import { ToDoList } from './../store/todos';
 import ToDolist from '../components/todoList';
+import Form from '../components/form/form';
 
 interface ILayoutProps {
   children?: React.ReactNode;
@@ -13,8 +14,8 @@ function Layout({ children }: ILayoutProps) {
   return (
     <div className={styles.container}>
       <div className={styles.body}>
-        <div style={{ width: '450px', backgroundColor: 'green' }}>
-          <Button store={mystore} />
+        <div style={{ width: '450px' }}>
+          <Form />
         </div>
         <div style={{ width: '450px' }}>
           <ToDolist store={mystore} />
