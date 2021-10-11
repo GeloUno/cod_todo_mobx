78518a22b19e5d22ed8ff187e0af91f7d88a76d5
GeloUno <geloke59@gmail.com>
2021-10-11 20:27:24 +0200

    Layout cleaning

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,21 +1,15 @@
 import React from 'react';
 import styles from './layout.module.css';
 import { mystore } from '../store/todos';
-import { Button } from '../components/todoList';
-// import { ToDoList } from './../store/todos';
 import ToDolist from '../components/todoList';
 import Form from '../components/form/form';
 
-interface ILayoutProps {
-  children?: React.ReactNode;
-}
-
-function Layout({ children }: ILayoutProps) {
+function Layout() {
   return (
     <div className={styles.container}>
       <div className={styles.body}>
         <div style={{ width: '450px' }}>
-          <Form />
+          <Form store={mystore} />
         </div>
         <div style={{ width: '450px' }}>
           <ToDolist store={mystore} />
