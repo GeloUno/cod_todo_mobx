b355ed24a2117c91fe83d698b062ea50a870c74c
GeloUno <geloke59@gmail.com>
2021-10-12 16:49:29 +0200

    LayOut change Form component to ToDoDetailSwitch

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -2,16 +2,18 @@ import React from 'react';
 import styles from './layout.module.css';
 import { mystore } from '../store/todos';
 import ToDolist from '../components/todoList';
-import Form from '../components/form/form';
+import InputToDo from '../components/inputToDo/inputToDo';
+import ToDoDetailSwitch from '../components/toDoDetailSwitch/ToDoDetailSwitch';
 
 function Layout() {
   return (
     <div className={styles.container}>
       <div className={styles.body}>
         <div style={{ width: '450px' }}>
-          <Form store={mystore} />
+          <ToDoDetailSwitch store={mystore} />
         </div>
         <div style={{ width: '450px' }}>
+          <InputToDo store={mystore} />
           <ToDolist store={mystore} />
         </div>
       </div>
