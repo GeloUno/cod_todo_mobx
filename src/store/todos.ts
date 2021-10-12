8bce3a4dcb002b2913c31ca74991a61f81a07e57
GeloUno <geloke59@gmail.com>
2021-10-12 16:48:33 +0200

    ToDo Store add method:
    setIdToDoDetailView
    getToDoById
    removeToDoById
    toggleShowOnEditForm

--- src/store/todos.ts
+++ src/store/todos.ts
@@ -3,25 +3,33 @@ import { makeAutoObservable } from 'mobx'
 // import { observer } from 'mobx-react-lite'
 import { nanoid } from 'nanoid'
 
-export interface IToDo {
-    id: string;
-    title: string;
-    description: string;
-    done: boolean
-}
+// export interface IToDo {
+//     id: string;
+//     title: string;
+//     description: string;
+//     done: boolean;
+//     dateCreate: Date;
+//     deadline: Date;
+// }
 
 export class ToDo {
     id: string;
     title: string;
     description: string;
-    done: boolean
+    done: boolean;
+    dateCreate: Date;
+    deadline: Date;
+    showAsEditForm: boolean;
 
-    constructor({ title, description, done, id }: { title: string, description: string, done: boolean, id: string }) {
+    constructor({ title, description, done, id, deadline }: { title: string, description: string, done: boolean, id: string, deadline: Date }) {
         makeAutoObservable(this);
         this.id = id;
         this.title = title;
         this.description = description;
-        this.done = done
+        this.done = done;
+        this.deadline = deadline;
+        this.dateCreate = new Date()
+        this.showAsEditForm = false
     }
 
     public toggleDoneTodo() {
@@ -29,35 +37,62 @@ export class ToDo {
     }
 }
 
+// export interface IToDoList {
+//     list: Array<ToDo>;
+//     idToDoDetailView: string;
+//     addToDo(todo: ToDo): void;
+//     toggleDone(id: string): void
+//     setIdToDoDetailView(id: string): void
+//     getToDoById(id: string): ToDo | undefined
+// }
+
 export class ToDoList {
-    list: Array<ToDo> = []
+    listToDo: Array<ToDo> = []
+    idToDoDetailView?: string;
     constructor(list: Array<ToDo>) {
         makeAutoObservable(this);
-        this.list = list;
+        this.listToDo = list;
     }
     addToDo(todo: ToDo) {
-        this.list.push(todo)
-        // console.log(`list`, this.list)
+        this.listToDo.push(todo)
     }
     toggleDone(id: string) {
-        this.list.find(li => {
+        this.listToDo.forEach(li => {
             if (li.id === id) {
                 li.done = !li.done
             }
         })
+    }
+    setIdToDoDetailView(id: string) {
+        console.log(`id`, id)
+        this.idToDoDetailView = id
+        console.log(`this.idToDoDetailView`, this.idToDoDetailView)
+    }
 
+    getToDoById(id: string): ToDo | undefined {
+        return this.listToDo.find(item => item.id === id)
+    }
+    removeToDoById(id: string) {
+        const data = this.listToDo.filter(item => item.id !== id)
+        this.listToDo = data
+    }
+    toggleShowOnEditForm(id: string) {
+        this.listToDo.forEach(li => {
+            if (li.id === id) {
+                li.showAsEditForm = !li.showAsEditForm
+            }
+        })
     }
 }
 
 
 export const storeList: Array<ToDo> = [
-    new ToDo({ description: 'example1', done: false, title: 'some1', id: nanoid() }),
-    new ToDo({ description: 'example2', done: true, title: 'some2', id: nanoid() }),
+    new ToDo({ description: 'example1', done: false, title: 'some1', id: "1", deadline: new Date('2021-10-10') }),
+    new ToDo({ description: 'example2', done: true, title: 'some2', id: "2", deadline: new Date('2022-01-01') }),
 ]
-// observable({ store })
 
 export const mystore: ToDoList = new ToDoList(storeList)
-// export const store = observable(store1)
+
 
 
 
