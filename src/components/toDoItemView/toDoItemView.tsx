d9e814b0d5bce22c49b9ec5395279d5197f7a291
GeloUno <geloke59@gmail.com>
2021-10-12 16:47:02 +0200

    ToDoItemView switch between details or shortcuts

--- src/components/toDoItemView/toDoItemView.tsx
+++ src/components/toDoItemView/toDoItemView.tsx
@@ -3,9 +3,30 @@ import { ToDo } from '../../store/todos';
 import { observer } from 'mobx-react-lite';
 import styles from './toDoItemView.module.css';
 import Button from '../button/button';
+import Moment from 'react-moment';
+
+interface ToDoProps extends Omit<ToDo, 'showAsEditForm'> {
+  isShowDetail: boolean;
+  //TODO: what should be better empty or optional ? :-/
+  setIdToDoDetailView(): void;
+  removeToDo(): void;
+  showInEditForm?(): void;
+}
 
 const ToDoItemView = observer(
-  ({ description, done, id, title, toggleDoneTodo }: ToDo) => {
+  ({
+    description,
+    done,
+    id,
+    title,
+    toggleDoneTodo,
+    dateCreate,
+    deadline,
+    isShowDetail,
+    setIdToDoDetailView,
+    removeToDo,
+    showInEditForm,
+  }: ToDoProps) => {
     const stylesBoxState = done ? styles.doneBox : styles.todoBox;
     const stylesTextState = done ? styles.doneText : styles.todoText;
     return (
@@ -15,15 +36,47 @@ const ToDoItemView = observer(
           <div>
             <h4 className={`${stylesTextState}`}>{title}</h4>
           </div>
-          <div>
-            <p className={`${stylesTextState}`}>{description}</p>
-          </div>
-          <Button onClick={toggleDoneTodo} variant="primary">
-            toggle Done
-          </Button>
-          <Button onClick={toggleDoneTodo} variant="danger">
-            Remove
-          </Button>
+          {isShowDetail && (
+            <div>
+              <p className={`${stylesTextState}`}>{description}</p>
+            </div>
+          )}
+          {isShowDetail && (
+            <div>
+              <p className={`${stylesTextState}`}>
+                Created at:{' '}
+                <strong>
+                  <Moment format="YYYY-DD-MM">{dateCreate}</Moment>
+                </strong>
+              </p>
+            </div>
+          )}
+          {isShowDetail && (
+            <div>
+              <p className={`${stylesTextState}`}>
+                Deadline:{' '}
+                <strong>
+                  <Moment format="YYYY-DD-MM">{deadline}</Moment>
+                </strong>
+              </p>
+            </div>
+          )}
+          {!isShowDetail && (
+            <Button onClick={setIdToDoDetailView} variant="secondary">
+              Show
+            </Button>
+          )}
+          {!isShowDetail && (
+            <Button onClick={showInEditForm} variant="primary">
+              Edit
+            </Button>
+          )}
+
+          {!isShowDetail && (
+            <Button onClick={removeToDo} variant="danger">
+              Remove
+            </Button>
+          )}
         </div>
       </div>
     );
