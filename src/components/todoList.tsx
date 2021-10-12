d643b3a1537886c5c3707d0197a7ce189430119f
GeloUno <geloke59@gmail.com>
2021-10-12 16:41:28 +0200

    ToDoList cleaning

--- src/components/todoList.tsx
+++ src/components/todoList.tsx
@@ -1,48 +1,35 @@
-import React from 'react';
-import { ToDo, ToDoList } from '../store/todos';
+import { ToDoList } from '../store/todos';
 import { observer } from 'mobx-react-lite';
-import { nanoid } from 'nanoid';
 import ToDoItemView from './toDoItemView/toDoItemView';
-
-interface TodoListProps {
-  toDoList: Array<ToDo>;
-}
-
-export const Button = observer(({ store }: { store: ToDoList }) => {
-  return (
-    <div>
-      <button
-        onClick={() =>
-          store.addToDo(
-            new ToDo({
-              description: 'example5',
-              done: false,
-              title: 'some5',
-              id: nanoid(),
-            })
-          )
-        }
-      >
-        Add
-      </button>
-    </div>
-  );
-});
+import InputToDo from './inputToDo/inputToDo';
 
 const ToDolist = observer(({ store }: { store: ToDoList }) => {
-  // console.log(`object`, store);
   return (
     <div>
-      {store.list.map((li) => (
-        <ToDoItemView
-          key={li.id}
-          description={li.description}
-          done={li.done}
-          id={li.id}
-          title={li.title}
-          toggleDoneTodo={() => store.toggleDone(li.id)}
-        />
-      ))}
+      {store.listToDo.map((li) => {
+        if (li.showAsEditForm) {
+          return <InputToDo store={store} />;
+        } else {
+          return (
+            <ToDoItemView
+              key={li.id}
+              description={li.description}
+              done={li.done}
+              id={li.id}
+              title={li.title}
+              toggleDoneTodo={() => store.toggleDone(li.id)}
+              dateCreate={li.dateCreate}
+              deadline={li.deadline}
+              isShowDetail={false}
+              setIdToDoDetailView={() => store.setIdToDoDetailView(li.id)}
+              removeToDo={() => store.removeToDoById(li.id)}
+              showInEditForm={() => {
+                store.toggleShowOnEditForm(li.id);
+              }}
+            />
+          );
+        }
+      })}
     </div>
   );
 });
